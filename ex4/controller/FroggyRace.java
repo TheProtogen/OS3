6a2fe385621a1080d03e56301bfbdae4c938a93f
TheProtogen <luepez357@gmail.com>
2023-09-12 15:11:03 -0300

    FroggyUpdate
    Novas funcionalidades para os sapos

--- ex4/controller/FroggyRace.java
+++ ex4/controller/FroggyRace.java
@@ -1,6 +1,5 @@
 package controller;
 
-import java.util.HashMap;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,26 +11,23 @@ public class FroggyRace extends Thread {
 	private static AtomicInteger froggyChamps = new AtomicInteger(1);; //Mostra a posição dos sapos que passaram da distancia máxima
 	private int distanciaMax; //Distancia máxima que o sapo deve percorrer
 	private int froggyNum; //Número atribuido ao sapo
-	
-	//Variáveis que podem ser modificadas
-	private int puloMax = 4; //Pulo máximo do sapo
+	private int puloMax; //Pulo máximo do sapo
 	private int froggyPos; //Posição atual do sapo
 	private int froggyJump; //Pulo que o sapo deu no momento
 	
-	public FroggyRace(int froggyNum, int distanciaMax) {
+	public FroggyRace(int froggyNum, int distanciaMax, int puloMax) {
 		this.froggyNum = froggyNum;
 		this.distanciaMax = distanciaMax;
-		
+		this.puloMax = puloMax;
 	}
 	
 	@Override
 	public void run() {
 		
-		
 		while (froggyPos < distanciaMax) {
 			froggyPos += froggyJump;
 			System.out.println(getId()+" : "+froggyJump+" : "+froggyPos);
-			froggyJump = froggyJump();
+			froggyJump = froggyPulo();
 			if (this.froggyPos > distanciaMax) {
 				interrupt();
 			}
@@ -41,10 +37,27 @@ public class FroggyRace extends Thread {
 				" metros  ["+posicaoChegada+"° lugar]");
 	}
 	
-	public int froggyJump() {
-		return random.nextInt(puloMax);
+	public int froggyPulo() {
+		int jump = random.nextInt(puloMax+1);
+		return jump;
 	}
 	
+	//  Mecânicas novas dos sapos. 
+	//     [UNDER CONSTRUCTION]
+	
+	
+	//Sabotagem = O sapo vai sabotar a próxima thread, podendo até ser ela mesma.
+	public int froggySabotagem() {
+		int sabotagem = random.nextInt(puloMax);
+		sabotagem = sabotagem - (sabotagem*2);
+		return sabotagem;
+	}
+	
+	//Boost = O sapo vai ganhar um boost.
+	public int froggyBoost(int jump) {
+		int boost = random.nextInt(puloMax);
+		return boost;
+	}
 	
 	
 }
--- ex4/view/Main4_view.java
+++ ex4/view/Main4_view.java
@@ -13,14 +13,17 @@ public class Main4_view {
 		//Quando o sapo alcançar a linha de chegada, mostrar posição
 		
 		//Variáveis que podem ser modificadas
-		int froggyNum = 5;
-		int pistaTamanho = 100;
+		
+		int froggyNum = 10; //Número de sapos
+		int pistaTamanho = 1000; //Distancia máxima que o sapo deve percorrer
+		int puloMax = 10; //Pulo máximo do sapo
+		
 		//===================================
 
 		Thread[] sapos = new Thread[froggyNum];
 		
 		for (int i = 0; i < froggyNum; i++) {
-			sapos[i] = new FroggyRace(i+1,pistaTamanho);
+			sapos[i] = new FroggyRace(i+1,pistaTamanho,puloMax);
 			sapos[i].start();
 		}
 	}
